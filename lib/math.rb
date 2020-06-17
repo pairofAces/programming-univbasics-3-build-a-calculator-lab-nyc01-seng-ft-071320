abaa2df98d84ae960bd73fe43b0c0ce56e3f2121
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-17 21:59:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  sub = num1 - num2
+  sub
 end
 
 def division(num1, num2)
-
+  divide = num1 / num2
+  divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+  multiply
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  mod
 end
 
 def square_root(num)
-
+  sqaure = Math.sqrt(num)
+  square
 end
